c2b825b559ba3d1ff1a75f97e8fb43c7206d52e7
Nivedita Gautam <niveditagautam000@gmail.com>
2018-12-30 18:28:13 -0600

    Added constructor to TxOperation

--- HelloWorld/src/operation/BeginTxOperation.java
+++ HelloWorld/src/operation/BeginTxOperation.java
@@ -4,7 +4,9 @@
 
 package operation;
 
+import entity.Transaction;
 import entity.TxOperation;
+import include.TxOperationType;
 
 /**
  * This class is BeginTx Operation thread. This thread will perform the
@@ -12,6 +14,15 @@ import entity.TxOperation;
  */
 public class BeginTxOperation  extends TxOperation{
 
+	public BeginTxOperation(Transaction ownerTx, TxOperationType txOpType, int txOpSeqNumber) {
+		
+		setOwnerTx(ownerTx);
+		setTxOpType(txOpType);
+		setTxOpSeqNumber(txOpSeqNumber);
+		
+		setOperationTime((int)(Math.random()*1000+1000));
+	}
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
--- HelloWorld/src/zgt/ZGTTransaction.java
+++ HelloWorld/src/zgt/ZGTTransaction.java
@@ -6,9 +6,16 @@
 
 package zgt;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import entity.SharedObject;
 import entity.Transaction;
 import include.LockMode;
+import include.LockTable;
+import include.TransactionManager;
 import include.TransactionStatus;
 import include.TransactionType;
 
@@ -20,18 +27,74 @@ public class ZGTTransaction extends Transaction {
 		super(id, status, type);
 		// TODO Auto-generated constructor stub
 	}
-	
 
 	@Override
 	public void addTx() {
-		
-		if (true);
-		
-	}	
-	
+
+		// this is the first tx
+		if (TransactionManager.firstTx == null) {
+
+			// set first and last tx as current tx since it's the only tx.
+			TransactionManager.firstTx = this;
+			TransactionManager.lastTx = this;
+
+			// update pointers
+			TransactionManager.firstTx.next = null;
+			TransactionManager.lastTx.next = null;
+
+		} // not first tx
+		else {
+
+			// add this tx to the list
+			TransactionManager.lastTx.next = this;
+			this.next = null;
+			// set current tx as the last tx
+			TransactionManager.lastTx = this;
+		}
+	}
+
 	@Override
 	public boolean removeTx() {
-		// TODO Auto-generated method stub
+
+		// check if the tx holds any locks
+		
+		boolean holdsLock = false;
+
+		Iterator<Entry<Integer, HashMap<Integer, LockMode>>> i = LockTable.LOCK_HASH_TABLE.entrySet().iterator();
+
+		while (i.hasNext()) {
+
+			Map.Entry<Integer, HashMap<Integer, LockMode>> HTEntry = i.next();
+
+			Integer objId = HTEntry.getKey();
+			HashMap<Integer, LockMode> valueHashMap = HTEntry.getValue();
+
+			Iterator<Entry<Integer, LockMode>> j = valueHashMap.entrySet().iterator();
+
+			while (j.hasNext()) {
+
+				Map.Entry<Integer, LockMode> valueEntry = j.next();
+
+				Integer txId = valueEntry.getKey();
+				
+				if (txId == this.getId()) {
+					
+					holdsLock = true;
+					break;
+				}
+			}
+		}
+		
+		// if the tx does not holds lock
+		if (!holdsLock) {
+			
+			
+			
+			
+			
+			
+		}
+
 		return false;
 	}
 
@@ -44,13 +107,13 @@ public class ZGTTransaction extends Transaction {
 	@Override
 	public void freeLocks() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void performReadWrite(LockMode lockMode, SharedObject sharedObject) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
